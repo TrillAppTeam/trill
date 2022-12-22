fe80abccf4d2b5383231c485d21fa13340be01af
Ashley Voglewede <avwede@gmail.com>
2022-12-22 12:41:53 -0500

    Add config file for Cognito

---
+++ web/src/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_CUiuR1mkZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6rp6iv3adtkkj9i1b7phfjb5fv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
