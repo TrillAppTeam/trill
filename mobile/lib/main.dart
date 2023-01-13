983c6ca77bc5086fab1f38fcbfb87de57046ec7a
prathik2001 <prathik.ramesh@knights.ucf.edu>
2023-01-12 22:18:56 -0500

    Added sidebar - basic UI iteration

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -354,12 +354,6 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Trill'),
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            // Open sidebar
-          },
-        ),
         actions: [IconButton(
           icon: Icon(Icons.search),
           onPressed: (){
@@ -369,6 +363,66 @@ class HomeScreen extends StatelessWidget {
             Navigator.pushNamed(context, '/search');
           },
         )],
+      ),
+      drawer: Drawer(
+        child: Container(
+          color: Colors.black,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              SizedBox(height: 70),
+                Column (
+                  children: [
+                    const Text( 'Matthew Gerber',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                  const Text('@GerbersGrumblings'),
+                  ]
+                ),
+              SizedBox(height: 50),
+              ListTile(
+                title: const Text('Home'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/home');
+                },
+              ),
+              ListTile(
+                title: const Text('Reviews'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+              ListTile(
+                title: const Text('Lists'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+              ListTile(
+                title: const Text('Likes'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+              SizedBox(height: 30),
+              ListTile(
+                title: const Text('Log Out'),
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.pushNamed(context, '/login');
+                },
+              ),
+            ],
+          ),
+        ),
       ),
       body: Container(
         padding: const EdgeInsets.all(16.0),
@@ -383,7 +437,6 @@ class HomeScreen extends StatelessWidget {
                     color: Colors.blue,
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Roboto',
                   ),
                 ),
                 SizedBox(height: 20),
@@ -394,7 +447,6 @@ class HomeScreen extends StatelessWidget {
                     color: Colors.white,
                     fontSize: 12,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Roboto',
                   ),
                 ),
                 SizedBox(height: 10),
@@ -407,7 +459,6 @@ class HomeScreen extends StatelessWidget {
                     color: Colors.white,
                     fontSize: 12,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Roboto',
                   ),
                 ),
                 SizedBox(height: 10),
@@ -420,7 +471,6 @@ class HomeScreen extends StatelessWidget {
                     color: Colors.white,
                     fontSize: 12,
                     fontWeight: FontWeight.bold,
-                    fontFamily: 'Roboto',
                   ),
                 ),
                 SizedBox(height: 10),
