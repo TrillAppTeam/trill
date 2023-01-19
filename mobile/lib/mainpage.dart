eb9751ed51cab59691955fa1fa3cb0f1bc34cafb
CathyChian <chiancathy@gmail.com>
2023-01-19 02:26:54 -0500

    bottom nav bar styling

--- mobile/lib/mainpage.dart
+++ mobile/lib/mainpage.dart
@@ -26,30 +26,46 @@ class _MainPageState extends State<MainPage> {
         index: currentIndex,
         children: screens,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF1A1B29),
-        selectedItemColor: Color(0xFFBC6AAB),
-        unselectedItemColor: Color(0xFFBC6AAB),
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Color(0xFFBC6AAB).withOpacity(.2),
+              blurRadius: 15,
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Color(0xFF1A1B29),
+          selectedItemColor: Color(0xFFBC6AAB),
+          unselectedItemColor: Color(0xFF888888),
+          showUnselectedLabels: false,
+          iconSize: 30,
+          elevation: 10,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: '____',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined),
+              activeIcon: Icon(Icons.search),
+              label: '____',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline_outlined),
+              activeIcon: Icon(Icons.person),
+              label: '____',
+            ),
+          ],
+        ),
       ),
     );
   }
