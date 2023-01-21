3841f1d7c4696b5964cbb6de542d4b958351fdde
avwede <avwede@gmail.com>
2023-01-21 18:27:39 -0500

    Add responseiveness to album art

--- web/src/pages/Landing.jsx
+++ web/src/pages/Landing.jsx
@@ -58,12 +58,12 @@ function Landing() {
 
             {/* Albums Section */}
             <section>
-                <div className="text-white flex flex-row justify-center gap-8 m-[10px] pb-[100px]">
-                    <div className="ring-2 ring-gray-500"><img src="/blondAlbum.jpg" width="210" /></div>
-                    <div className="ring-2 ring-gray-500"><img src="/allThingsMustPassAlbum.jpg" width="210" /></div>
-                    <div className="ring-2 ring-gray-500"><img src="/currentsAlbum.jpg" width="210" /></div>
-                    <div className="ring-2 ring-gray-500"><img src="/whereTheLightIsAlbum.jpg" width="210" /></div>
-                    <div className="ring-2 ring-gray-500"><img src="/frontiersAlbum.jpg" width="210
+                <div className="text-white flex flex-row justify-center gap-8 m-[10px] pb-[100px] 0">
+                    <div className="ring-2 ring-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110" data-aos="fade-up"><img src="/blondAlbum.jpg" width="210" /></div>
+                    <div className="ring-2 ring-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"><img src="/allThingsMustPassAlbum.jpg" width="210" /></div>
+                    <div className="ring-2 ring-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"><img src="/currentsAlbum.jpg" width="210" /></div>
+                    <div className="ring-2 ring-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"><img src="/whereTheLightIsAlbum.jpg" width="210" /></div>
+                    <div className="ring-2 ring-gray-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110"><img src="/frontiersAlbum.jpg" width="210
                     "/></div>
                 </div>
             </section>
