31af94b5d5aa04a8f0a9c683288e15502f044083
avwede <avwede@gmail.com>
2023-01-24 12:43:28 -0500

    Fix navbar colors, add opening line to home page

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -11,8 +11,10 @@ function Home() {
     const {userName, firstName, lastName, email} = userData;
     return (
         <div className="text-white">
-            <p>Home Page</p>
-            <h1>Welcome back, {firstName}. Here's what the world has been listening to.</h1>
+            <h1 className="font-bold text-5xl md:text-5xl text-white text-center pt-[20px]"> Welcome back,
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-teal-300"> {firstName}. </span><br/>
+                <h2 className="text-4xl md:text-4xl py-[15px]">Here's what the world has been listening to.</h2>
+            </h1>
 
             
             <h1>Popular Albums This Week (Globally)</h1>
