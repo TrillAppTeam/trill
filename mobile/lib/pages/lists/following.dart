e03a3d6d75cd4e3f94eb0f12df7fc74c1cd8ce1a
prathik2001 <prathik.ramesh@knights.ucf.edu>
2023-01-23 22:33:02 -0500

    Added some list screens

--- mobile/lib/pages/lists/following.dart
+++ mobile/lib/pages/lists/following.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:trill/models/user.dart';
 
+// Currently same as class FollowerScreen - will change once API call added
 class FollowingScreen extends StatelessWidget {
+  final List<User> Users = [
+    User(name: 'John Doe', handle: '@johndoe'),
+    User(name: 'John Cena', handle: '@johncena'),
+    User(name: 'Willem Dafoe', handle: '@WillemDafoeIsVeryCool'),
+    User(name: 'Pablo', handle: '@MountainManPablo'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Following'),
+      ),
+      body: ListView.builder(
+        itemCount: Users.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onTap: () {
+              // navigate to their profile, pass the associated User object
+            },
+            title: Text(
+              Users[index].name,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: Text(Users[index].handle),
+            trailing: Icon(Icons.arrow_forward_outlined, color: Colors.white.withOpacity(0.2))
+          );
+        },
       ),
     );
   }
