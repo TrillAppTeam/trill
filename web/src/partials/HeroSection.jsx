058538fa8f5bf3a261ab109e1b5d7dcbdcbd2526
Ashley <avwede@gmail.com>
2023-01-27 10:16:32 -0500

    Update Auth Links (#18)

--- web/src/partials/HeroSection.jsx
+++ web/src/partials/HeroSection.jsx
@@ -9,11 +9,14 @@ function HeroSection() {
                 </div>
 
                 <div className="absolute right-0 lg:flex lg:items-center lg:w-auto items-start z-10 pt-6 pr-10">
-                    <a href="https://auth.trytrill.com/signup?client_id=126gi8rqrvn4rbv1kt7ef714oa&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.trytrill.com%2Fhome"
+                    <a href="https://auth.trytrill.com/login?client_id=126gi8rqrvn4rbv1kt7ef714oa&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.trytrill.com%2Fhome"
                         className="font-bold text-lg text-slate-100 p-5 hover:text-trillBlue">
                         LOGIN
                     </a>
-                    <Link to="CreateAccount" className="font-bold text-lg text-slate-100 p-5 hover:text-trillBlue"> CREATE ACCOUNT </Link>
+                    <a href="https://auth.trytrill.com/signup?client_id=126gi8rqrvn4rbv1kt7ef714oa&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fwww.trytrill.com%2Fhome"
+                        className="font-bold text-lg text-slate-100 p-5 hover:text-trillBlue">
+                        CREATE ACCOUNT
+                    </a>
                 </div>
 
                 {/* Main Hero Image - Abbey Road */}
