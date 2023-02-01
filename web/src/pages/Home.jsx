583a4a47658aed110c07fe45ba51756190e31e9a
DMFlo <dmflorez23@gmail.com>
2023-02-01 00:51:02 -0500

    Basic api call to request token

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -2,30 +2,65 @@ import React, { useEffect } from 'react';
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { QueryClient, QueryClientProvider, useQuery} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+import axios from 'axios';
 
 function Home() {
     useEffect (() => {
         let token;
         try {
             token = (window.location.href).split('=')[1];
-        } catch {
-            console.log("Error: No auth token found.");
-        } finally {
             console.log(token);
-            //api call
-        };
+            // let result = useQuery({
+            //     queryKey: ['auth'], 
+            //     queryFn: axios({
+            //         method: 'post',
+            //         url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
+            //         headers: {
+            //             'Content-Type': 'application/x-www-form-urlencoded'
+            //         }, 
+            //         data: {
+            //           grant_type: 'authorization_code',
+            //           client_id: '126gi8rqrvn4rbv1kt7ef714oa',
+            //           code: token,
+            //           redirect_uri: 'https://www.trytrill.com/home',
+            //         }
+            // })})
+            let result = axios({
+                        method: 'post',
+                        url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
+                        headers: {
+                            'Content-Type': 'application/x-www-form-urlencoded'
+                        }, 
+                        data: {
+                          grant_type: 'authorization_code',
+                          client_id: '126gi8rqrvn4rbv1kt7ef714oa',
+                          code: token,
+                          redirect_uri: 'https://www.trytrill.com/home',
+                        }
+                }).then((res) => res.data);
+            console.group(result);
+        } catch (e) {
+            console.log(e.toString());
+            return;
+        }
     }, []);
 
     return (
-        <div className="bg-trillPurple min-h-screen flex flex-col">
-            <Navbar />
+        <QueryClientProvider client={queryClient}>
+            <div className="bg-trillPurple min-h-screen flex flex-col">
+                <Navbar />
 
-            {/* Page Content */}
-            <div className="flex-grow">
-                <Outlet />
-            </div>
-            <Footer />
-        </div>
+                {/* Page Content */}
+                <div className="flex-grow">
+                    <Outlet />
+                </div>
+                <Footer />
+            </div>  
+        </QueryClientProvider>
     );
 }
 
