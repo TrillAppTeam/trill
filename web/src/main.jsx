8762297421dc0e380443d3084df8990898cf6948
DMFlo <dmflorez23@gmail.com>
2023-02-01 22:57:41 -0500

    Home calls and stores jwt after login

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -20,6 +20,10 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -81,6 +85,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,66 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { QueryClient, QueryClientProvider, useQuery} from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
-
+import { useQuery } from "@tanstack/react-query";
 import axios from 'axios';
 
 function Home() {
-    useEffect (() => {
-        let token;
-        try {
-            token = (window.location.href).split('=')[1];
-            console.log(token);
-            // let result = useQuery({
-            //     queryKey: ['auth'], 
-            //     queryFn: axios({
-            //         method: 'post',
-            //         url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
-            //         headers: {
-            //             'Content-Type': 'application/x-www-form-urlencoded'
-            //         }, 
-            //         data: {
-            //           grant_type: 'authorization_code',
-            //           client_id: '126gi8rqrvn4rbv1kt7ef714oa',
-            //           code: token,
-            //           redirect_uri: 'https://www.trytrill.com/home',
-            //         }
-            // })})
-            let result = axios({
-                        method: 'post',
-                        url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
-                        headers: {
-                            'Content-Type': 'application/x-www-form-urlencoded'
-                        }, 
-                        data: {
-                          grant_type: 'authorization_code',
-                          client_id: '126gi8rqrvn4rbv1kt7ef714oa',
-                          code: token,
-                          redirect_uri: 'https://www.trytrill.com/home',
-                        }
-                }).then((res) => res.data);
-            console.group(result);
-        } catch (e) {
-            console.log(e.toString());
-            return;
-        }
-    }, []);
+    const { isLoading, error, data } = useQuery(['fetchToken'], () => 
+        axios({
+            method: 'post',
+            url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }, 
+            data: {
+                grant_type: 'authorization_code',
+                client_id: '126gi8rqrvn4rbv1kt7ef714oa',
+                code: (window.location.href).split('=')[1],
+                redirect_uri: 'https://www.trytrill.com/home',
+            }}).then((res) => {
+                localStorage.setItem('access_token', res.data.access_token);
+                return res;
+            }),
+        { refetchOnWindowFocus: false });
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <div className="bg-trillPurple min-h-screen flex flex-col">
+        <div className="bg-trillPurple min-h-screen flex flex-col">
+            {isLoading ? "Loading..." : <>
                 <Navbar />
-
                 {/* Page Content */}
                 <div className="flex-grow">
                     <Outlet />
                 </div>
                 <Footer />
-            </div>  
-        </QueryClientProvider>
+            </>}
+        </div>  
     );
 }
 
