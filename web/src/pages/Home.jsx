9db020efe9069e9b1939e1f35844d53b649e64c9
DMFlo <70411272+DMFlo@users.noreply.github.com>
2023-02-02 17:30:41 +0000

    JWT token store (#26)
    * Basic api call to request token
    
    * Home calls and stores jwt after login

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,31 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useQuery } from "@tanstack/react-query";
+import axios from 'axios';
 
 function Home() {
-    useEffect (() => {
-        let token;
-        try {
-            token = (window.location.href).split('=')[1];
-        } catch {
-            console.log("Error: No auth token found.");
-        } finally {
-            console.log(token);
-            //api call
-        };
-    }, []);
+    const { isLoading, error, data } = useQuery(['fetchToken'], () => 
+        axios({
+            method: 'post',
+            url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }, 
+            data: {
+                grant_type: 'authorization_code',
+                client_id: '126gi8rqrvn4rbv1kt7ef714oa',
+                code: (window.location.href).split('=')[1],
+                redirect_uri: 'https://www.trytrill.com/home',
+            }}).then((res) => {
+                localStorage.setItem('access_token', res.data.access_token);
+                return res;
+            }),
+        { refetchOnWindowFocus: false });
 
     return (
         <div className="bg-trillPurple min-h-screen flex flex-col">
-            <Navbar />
-
-            {/* Page Content */}
-            <div className="flex-grow">
-                <Outlet />
-            </div>
-            <Footer />
-        </div>
+            {isLoading ? "Loading..." : <>
+                <Navbar />
+                {/* Page Content */}
+                <div className="flex-grow">
+                    <Outlet />
+                </div>
+                <Footer />
+            </>}
+        </div>  
     );
 }
 
