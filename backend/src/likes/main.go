48b9f306d2991c31bea667a7f47d5e1d1c4cee71
CathyChian <chiancathy@gmail.com>
2023-02-07 23:31:04 -0500

    getLikeCount and unlike APIs

--- backend/src/likes/main.go
+++ backend/src/likes/main.go
@@ -72,12 +72,12 @@ func connectDB() (*gorm.DB, error) {
 
 func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
 	switch req.RequestContext.HTTP.Method {
-	// case "GET":
-	// 	return getLikes(ctx, req)
+	case "GET":
+		return getLikeCount(ctx, req)
 	case "PUT":
 		return like(ctx, req)
-	// case "DELETE":
-	// 	return unlike(ctx, req)
+	case "DELETE":
+		return unlike(ctx, req)
 	default:
 		err := fmt.Errorf("HTTP Method '%s' not allowed", req.RequestContext.HTTP.Method)
 		return Response{StatusCode: 405, Body: err.Error()}, err
@@ -85,10 +85,38 @@ func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response,
 }
 
 // Given a reviewID, get the number of likes for that review
-// func getLikes(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
-// }
+// @PARAMS are QueryStringParameters: "review_id"
+// Postman: GET - /likes?review_id={reviewID}
+func getLikeCount(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
+	db, err := connectDB()
+	if err != nil {
+		return Response{StatusCode: 500, Body: err.Error()}, err
+	}
+
+	// Get the review ID
+	reviewID, ok := req.QueryStringParameters["review_id"]
+	if !ok {
+		return Response{StatusCode: 500, Body: "Failed to parse review ID"}, nil
+	}
+
+	// Given the review ID, find the users who liked it
+	var likeUsers []Like
+	var result = db.Where("review_id = ?", reviewID).Find(&likeUsers)
+	if err := result.Error; err != nil {
+		return Response{StatusCode: 500, Body: err.Error()}, err
+	}
+
+	// Serialize users into JSON; Return usersJSON if we want the list of users
+	// usersJSON, err := json.Marshal(likeUsers)
+	// if err != nil {
+	// 	return Response{StatusCode: 500, Body: err.Error()}, err
+	// }
+
+	return Response{StatusCode: 200, Body: fmt.Sprint(result.RowsAffected)}, nil
+}
 
 // User likes a review
+// Postman: PUT - /likes
 func like(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
 	db, err := connectDB()
 	if err != nil {
@@ -116,9 +144,31 @@ func like(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, er
 }
 
 // User unlikes a review
-// func unlike(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
+// Postman: DELETE - /likes
+func unlike(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
+	db, err := connectDB()
+	if err != nil {
+		return Response{StatusCode: 500, Body: err.Error()}, err
+	}
 
-// }
+	// Create Like to be deleted
+	likeToDelete := new(Like)
+	err = json.Unmarshal([]byte(req.Body), &likeToDelete)
+	if err != nil {
+		return Response{StatusCode: 400, Body: "Invalid request data format"}, err
+	}
+
+	// Delete the requested Like from the database
+	if err := db.Where("username = ? AND review_id = ?", &likeToDelete.Username, &likeToDelete.ReviewID).Delete(&likeToDelete).Error; err != nil {
+		return Response{StatusCode: 404, Body: "Cannot delete follow relationship."}, nil
+	}
+
+	return Response{
+		StatusCode: 201,
+		Body: fmt.Sprintf("Successfully removed like to review %d from %s to database.",
+			likeToDelete.ReviewID, likeToDelete.Username),
+	}, nil
+}
 
 func main() {
 	lambda.Start(handler)
