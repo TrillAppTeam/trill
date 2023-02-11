b3de301f7f34bea56292f5f592aa7036a7605a2f
prathik2001 <prathik.ramesh@knights.ucf.edu>
2023-02-10 19:03:43 -0500

    Added some Favorite Albums endpoints

--- backend/src/album/main.go
+++ backend/src/album/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
-	"os"
 	"io"
-	"bytes"
+	"os"
 
 	"encoding/json"
 	"net/http"
+	"net/url"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -20,51 +21,47 @@ import (
 const SECRETS_PATH = "../../.secrets.yml"
 
 type Secrets struct {
-	host     string `yaml:"MYSQLHOST"`
-	port     string `yaml:"MYSQLPORT"`
-	database string `yaml:"MYSQLDATABASE"`
-	user     string `yaml:"MYSQLUSER"`
-	password string `yaml:"MYSQLPASS"`
-	region   string `yaml:"AWS_DEFAULT_REGION"`
-	spotifyID   string `yaml:"SPOTIFY_CLIENT_ID"`
-	spotifySecret   string `yaml:"SPOTIFY_CLIENT_SECRET"`
+	host          string `yaml:"MYSQLHOST"`
+	port          string `yaml:"MYSQLPORT"`
+	database      string `yaml:"MYSQLDATABASE"`
+	user          string `yaml:"MYSQLUSER"`
+	password      string `yaml:"MYSQLPASS"`
+	region        string `yaml:"AWS_DEFAULT_REGION"`
+	spotifyID     string `yaml:"SPOTIFY_CLIENT_ID"`
+	spotifySecret string `yaml:"SPOTIFY_CLIENT_SECRET"`
 }
 
 type SpotifyAlbums struct {
-	Albums struct {
-		Href  string `json:"href"`
-		Items []struct {
-			AlbumType      string `json:"album_type"`
-			ExternalUrls   struct {
-				Spotify string `json:"spotify"`
-			} `json:"external_urls"`
-			Href   string `json:"href"`
-			ID     string `json:"id"`
-			Images []struct {
-				URL    string `json:"url"`
-				Height int    `json:"height"`
-				Width  int    `json:"width"`
-			} `json:"images"`
-			Name         string   `json:"name"`
-			ReleaseDate  string   `json:"release_date"`
-			Type         string   `json:"type"`
-			URI          string   `json:"uri"`
-			Genres       []string `json:"genres"`
-			Label        string   `json:"label"`
-			Popularity   int      `json:"popularity"`
-			Artists      []struct {
-				ID   string `json:"id"`
-				Name string `json:"name"`
-				Type string `json:"type"`
-				URI  string `json:"uri"`
-			} `json:"artists"`
-		} `json:"items"`
-		Limit    int `json:"limit"`
-		Next     string `json:"next"`
-		Offset   int `json:"offset"`
-		Previous string `json:"previous"`
-		Total    int `json:"total"`
-	} `json:"albums"`
+	AlbumType    string `json:"album_type"`
+	ExternalUrls struct {
+		Spotify string `json:"spotify"`
+	} `json:"external_urls"`
+	Href   string `json:"href"`
+	ID     string `json:"id"`
+	Images []struct {
+		URL    string `json:"url"`
+		Height int    `json:"height"`
+		Width  int    `json:"width"`
+	} `json:"images"`
+	Name        string   `json:"name"`
+	ReleaseDate string   `json:"release_date"`
+	Type        string   `json:"type"`
+	URI         string   `json:"uri"`
+	Genres      []string `json:"genres"`
+	Label       string   `json:"label"`
+	Popularity  int      `json:"popularity"`
+	Artists     []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+		URI  string `json:"uri"`
+	} `json:"artists"`
+}
+
+type SpotifyToken struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
 }
 
 var secrets = Secrets{
@@ -91,37 +88,43 @@ func connectDB() (*gorm.DB, error) {
 	}
 }
 
-func handler(ctx context.Context, req events.APIGatewayProxyRequest) (Response, error) {
-	switch req.HTTPMethod {
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
+	switch req.RequestContext.HTTP.Method {
 	//case "POST":
 	//	return create(ctx, req)
 	case "GET":
 		return read(req)
 	//case "PUT":
 	//	return update(req)
-	// EDIT: we dont think a user should be able to delete their account <3
 	// case "DELETE":
 	// 	return delete(req)
 	default:
-		err := fmt.Errorf("HTTP Method '%s' not allowed", req.HTTPMethod)
+		err := fmt.Errorf("HTTP Method '%s' not allowed", req.RequestContext.HTTP.Method)
 		return Response{StatusCode: 405, Body: err.Error()}, err
 	}
 }
 
 // GET: Returns album info
-func read(req events.APIGatewayProxyRequest) (Response, error) {
+func read(req events.APIGatewayV2HTTPRequest) (Response, error) {
 	var resp SpotifyAlbums
 	var buf bytes.Buffer
 
-	albumID := req.QueryStringParameters["ID"]
 	clientSecret := secrets.spotifySecret
 	clientID := secrets.spotifyID
 
+	albumID, ok := req.QueryStringParameters["ID"]
+	if !ok {
+		return Response{StatusCode: 500, Body: "Failed to parse ID"}, nil
+	}
+
 	client := &http.Client{}
+	body := url.Values{}
+	body.Set("grant_type", "client_credentials")
+	body.Set("client_id", clientID)
+	body.Set("client_secret", clientSecret)
 
-	reqBody := []byte(fmt.Sprintf("grant_type=client_credentials&client_id=%s&client_secret=%s", clientID, clientSecret))
-
-	request, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBuffer(reqBody))
+	reqBody := bytes.NewBufferString(body.Encode())
+	request, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", reqBody)
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
@@ -139,22 +142,26 @@ func read(req events.APIGatewayProxyRequest) (Response, error) {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 
-	var AccessToken string
-	err = json.Unmarshal(buf.Bytes(), &AccessToken)
+	var token SpotifyToken
+	err = json.Unmarshal(buf.Bytes(), &token)
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
+
+	fmt.Println(token.AccessToken)
 
 	request, err = http.NewRequest("GET", "https://api.spotify.com/v1/albums/"+albumID, nil)
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
-	request.Header.Add("Authorization", "Bearer " + AccessToken)
+	request.Header.Add("Authorization", "Bearer "+token.AccessToken)
 	r, err := client.Do(request)
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 	defer r.Body.Close()
+
+	buf.Reset()
 
 	_, err = io.Copy(&buf, r.Body)
 	if err != nil {
@@ -166,8 +173,7 @@ func read(req events.APIGatewayProxyRequest) (Response, error) {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 
-	albums := resp.Albums.Items
-	albumsJson, err := json.Marshal(albums)
+	albumsJson, err := json.Marshal(resp)
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
--- backend/src/favoritealbums/main.go
+++ backend/src/favoritealbums/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
+	"io"
 	"os"
+
+	"encoding/json"
+
+	"net/http"
+	"net/url"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -18,12 +24,14 @@ const SECRETS_PATH = "../../.secrets.yml"
 type Response events.APIGatewayProxyResponse
 
 type Secrets struct {
-	host     string `yaml:"MYSQLHOST"`
-	port     string `yaml:"MYSQLPORT"`
-	database string `yaml:"MYSQLDATABASE"`
-	user     string `yaml:"MYSQLUSER"`
-	password string `yaml:"MYSQLPASS"`
-	region   string `yaml:"AWS_DEFAULT_REGION"`
+	host          string `yaml:"MYSQLHOST"`
+	port          string `yaml:"MYSQLPORT"`
+	database      string `yaml:"MYSQLDATABASE"`
+	user          string `yaml:"MYSQLUSER"`
+	password      string `yaml:"MYSQLPASS"`
+	region        string `yaml:"AWS_DEFAULT_REGION"`
+	spotifyID     string `yaml:"SPOTIFY_CLIENT_ID"`
+	spotifySecret string `yaml:"SPOTIFY_CLIENT_SECRET"`
 }
 
 var secrets = Secrets{
@@ -33,13 +41,55 @@ var secrets = Secrets{
 	os.Getenv("MYSQLUSER"),
 	os.Getenv("MYSQLPASS"),
 	os.Getenv("AWS_DEFAULT_REGION"),
+	os.Getenv("SPOTIFY_CLIENT_ID"),
+	os.Getenv("SPOTIFY_CLIENT_SECRET"),
 }
 
 type FavoriteAlbums struct {
-	gorm.Model
-	UserID  int    // `sql:"int"`
-	AlbumID string // `sql:"char(128)"
+	username string // ``gorm:"primarykey;unique"``
+	album_id string //
 }
+
+type SpotifyAlbums struct {
+	AlbumType    string `json:"album_type"`
+	ExternalUrls struct {
+		Spotify string `json:"spotify"`
+	} `json:"external_urls"`
+	Href   string `json:"href"`
+	ID     string `json:"id"`
+	Images []struct {
+		URL    string `json:"url"`
+		Height int    `json:"height"`
+		Width  int    `json:"width"`
+	} `json:"images"`
+	Name        string   `json:"name"`
+	ReleaseDate string   `json:"release_date"`
+	Type        string   `json:"type"`
+	URI         string   `json:"uri"`
+	Genres      []string `json:"genres"`
+	Label       string   `json:"label"`
+	Popularity  int      `json:"popularity"`
+	Artists     []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+		URI  string `json:"uri"`
+	} `json:"artists"`
+}
+
+type SpotifyToken struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int    `json:"expires_in"`
+}
+
+/*type User struct {
+	Username       string `gorm:"primarykey;unique"`
+	Bio            string
+	ProfilePicture string
+	Followers      []Follows `gorm:"foreignkey:Followee"`
+	Following      []Follows `gorm:"foreignkey:Following"`
+}*/
 
 // https://github.com/gugazimmermann/fazendadojuca/blob/master/animals/main.go
 
@@ -52,8 +102,8 @@ func connectDB() (*gorm.DB, error) {
 	}
 }
 
-func handler(ctx context.Context, req events.APIGatewayProxyRequest) (Response, error) {
-	switch req.HTTPMethod {
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
+	switch req.RequestContext.HTTP.Method {
 	case "POST":
 		return create(ctx, req)
 	case "GET":
@@ -61,26 +111,29 @@ func handler(ctx context.Context, req events.APIGatewayProxyRequest) (Response, 
 	case "DELETE":
 		return delete(req)
 	default:
-		err := fmt.Errorf("HTTP Method '%s' not allowed", req.HTTPMethod)
+		err := fmt.Errorf("HTTP Method '%s' not allowed", req.RequestContext.HTTP.Method)
 		return Response{StatusCode: 405, Body: err.Error()}, err
 	}
 }
 
 // User adds an album to their favorite albums. Only allows users to add an album if they have < 4 albums currently in their favorites.
-func create(ctx context.Context, req events.APIGatewayProxyRequest) (Response, error) {
+// @PARAMS - username (string), albumId (string)
+func create(ctx context.Context, req events.APIGatewayV2HTTPRequest) (Response, error) {
 	db, err := connectDB()
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
+	// db.AutoMigrate(&User{})
+	// db.AutoMigrate(&Follows{})
 
-	userID := req.QueryStringParameters["userID"]
-	if userID == "" {
-		return Response{StatusCode: 400, Body: "userID parameter is required"}, nil
+	username, ok := req.QueryStringParameters["username"]
+	if !ok {
+		return Response{StatusCode: 500, Body: "Failed to parse username"}, nil
 	}
 
 	// Only allow users to add an album if they have less than 4 albums currently in their favorites
 	var count int64
-	if err := db.Model(&FavoriteAlbums{}).Where("userID = ?", userID).Count(&count).Error; err != nil {
+	if err := db.Table("favorite_albums").Where("username = ?", username).Count(&count).Error; err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 
@@ -89,65 +142,152 @@ func create(ctx context.Context, req events.APIGatewayProxyRequest) (Response, e
 		return Response{StatusCode: 400, Body: err.Error()}, err
 	}
 
-	// Parse the JSON body
-	var album FavoriteAlbums
-	if err := json.Unmarshal([]byte(req.Body), &album); err != nil {
-		return Response{StatusCode: 400, Body: err.Error()}, err
+	// TO DO: Update database to be uuid instead of username, reflect here
+	// Unmarshal JSON request body into a Follows struct
+	fave_albums := new(FavoriteAlbums)
+	err = json.Unmarshal([]byte(req.Body), &fave_albums)
+	if err != nil {
+		return Response{StatusCode: 400, Body: "Invalid request data format"}, err
 	}
 
-	// Save the album to the database
-	if err := db.Create(&album).Error; err != nil {
-		return Response{StatusCode: 500, Body: err.Error()}, err
+	// Add the follow relationship to the database
+	err = db.Create(&fave_albums).Error
+	if err != nil {
+		return Response{StatusCode: 500, Body: "Error inserting data into database"}, err
 	}
 
-	return Response{StatusCode: 201, Body: "Successfully added album to favorites."}, nil
+	// Woo Hoo !!!
+	return Response{
+		StatusCode: 201,
+		Body:       "Successfully added to database",
+	}, nil
 }
 
-// Get's the user's favorite albums
-func read(req events.APIGatewayProxyRequest) (Response, error) {
+// Gets the user's favorite albums
+// @PARAMS - username (string)
+func read(req events.APIGatewayV2HTTPRequest) (Response, error) {
 	db, err := connectDB()
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 
-	userID := req.QueryStringParameters["userID"]
-	if userID == "" {
-		return Response{StatusCode: 400, Body: "userID parameter is required"}, nil
+	// Get the username from the JSON request body
+	username, ok := req.QueryStringParameters["username"]
+	if !ok {
+		return Response{StatusCode: 500, Body: "Failed to parse username"}, nil
 	}
 
-	// Get the user's favorite albums
-	var albums []FavoriteAlbums
-	if err := db.Where("userID = ?", userID).Find(&albums).Error; err != nil {
+	// Given the username, find
+	var fave_albums []FavoriteAlbums
+	if err := db.Where("username = ?", username).Find(&fave_albums).Error; err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 
-	// Convert the albums to JSON
-	albumsJSON, err := json.Marshal(albums)
-	if err != nil {
-		return Response{StatusCode: 500, Body: err.Error()}, err
+	albumInfo := make([]SpotifyAlbums, len(fave_albums))
+
+	for i := range fave_albums {
+		album, err := getAlbumInfo(fave_albums[i].album_id)
+		if err != nil {
+			return Response{StatusCode: 500, Body: err.Error()}, err
+		}
+		albumInfo[i] = album
 	}
 
-	return Response{StatusCode: 200, Body: string(albumsJSON)}, nil
+	albumInfoJSON, err := json.Marshal(albumInfo)
+
+	return Response{StatusCode: 200, Body: string(albumInfoJSON)}, nil
 }
 
-// User deletes an album from their favorite albums
-func delete(req events.APIGatewayProxyRequest) (Response, error) {
+func getAlbumInfo(albumID string) (SpotifyAlbums, error) {
+	var resp SpotifyAlbums
+	var buf bytes.Buffer
+
+	clientSecret := secrets.spotifySecret
+	clientID := secrets.spotifyID
+
+	client := &http.Client{}
+	body := url.Values{}
+	body.Set("grant_type", "client_credentials")
+	body.Set("client_id", clientID)
+	body.Set("client_secret", clientSecret)
+
+	reqBody := bytes.NewBufferString(body.Encode())
+	request, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", reqBody)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	tokenResp, err := client.Do(request)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+	defer tokenResp.Body.Close()
+
+	_, err = io.Copy(&buf, tokenResp.Body)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+
+	var token SpotifyToken
+	err = json.Unmarshal(buf.Bytes(), &token)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+
+	fmt.Println(token.AccessToken)
+
+	request, err = http.NewRequest("GET", "https://api.spotify.com/v1/albums/"+albumID, nil)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+	request.Header.Add("Authorization", "Bearer "+token.AccessToken)
+	r, err := client.Do(request)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+	defer r.Body.Close()
+
+	buf.Reset()
+
+	_, err = io.Copy(&buf, r.Body)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+
+	err = json.Unmarshal(buf.Bytes(), &resp)
+	if err != nil {
+		return SpotifyAlbums{}, err
+	}
+
+	return resp, nil
+}
+
+// Deletes an album from a user's favorite albums
+// @PARAMS - username (string), albumId (string)
+func delete(req events.APIGatewayV2HTTPRequest) (Response, error) {
 	db, err := connectDB()
 	if err != nil {
 		return Response{StatusCode: 500, Body: err.Error()}, err
 	}
 
-	albumID := req.QueryStringParameters["albumID"]
-	if albumID == "" {
-		return Response{StatusCode: 400, Body: "albumID parameter is required"}, nil
+	deleteRecord := new(FavoriteAlbums)
+	err = json.Unmarshal([]byte(req.Body), &deleteRecord)
+	if err != nil {
+		return Response{StatusCode: 400, Body: "Invalid request data format"}, err
 	}
 
-	// Delete the album from the database
-	if err := db.Delete(&albumID).Error; err != nil {
-		return Response{StatusCode: 500, Body: err.Error()}, err
+	// Find these two values in the database, then delete the record.
+	if err := db.Where("username = ? AND album_id = ?", &deleteRecord.username, &deleteRecord.album_id).Delete(&deleteRecord).Error; err != nil {
+		return Response{StatusCode: 404, Body: "Cannot delete favorited album."}, nil
 	}
 
-	return Response{StatusCode: 200, Body: "Successfully deleted album from favorites."}, nil
+	// Woo Hoo !!!
+	return Response{
+		StatusCode: 201,
+		Body:       "Successfully removed from database",
+	}, nil
 }
 
 func main() {
