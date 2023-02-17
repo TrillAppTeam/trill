c38846dd933aadfe042d520e5be07a35442bec01
avwede <avwede@gmail.com>
2023-02-16 21:32:23 -0500

    Adding team picks to landing

--- web/src/components/Loading.jsx
+++ web/src/components/Loading.jsx
@@ -1,7 +1,7 @@
 function Loading() {
     return (
         <div className="grid place-items-center h-screen">
-            <div className="w-20 h-20 border-4 border-dashed rounded-full animate-[spin_3s_linear_infinite] dark:border-teal-400"></div> 
+            <div className="w-20 h-20 border-4 border-dashed rounded-full animate-[spin_3s_linear_infinite] dark:border-trillBlue"></div> 
         </div>
         
     );
