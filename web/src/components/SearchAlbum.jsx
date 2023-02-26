f3d92a1fd1c0be616cebf053d9255c49288a784c
Ashley <avwede@gmail.com>
2023-02-26 18:32:01 -0500

    Add SearchAlbum component (#59)

--- web/src/components/SearchAlbum.jsx
+++ web/src/components/SearchAlbum.jsx
@@ -1,5 +1,40 @@
-function SearchAlbum() {
-    return();
+// Components
+import Album from "../components/Album"
+import SpotifySVG from "/spotify.svg"
+
+function SearchAlbum(props) {
+    const { img, name, year, artist, spotifyLink } = props.album;
+    
+    return (
+        <div className="max-w-6xl mx-auto">
+
+            <div className="flex flex-row">
+
+                {/* Album Art */}
+                <div className="p-3 m-2">
+                    <Album album = {{ img : img, size : "100", name : name }} />
+                </div>
+
+                <div className="flex flex-col p-3 gap-2 w-4/5">
+                    <div className="flex flex-row gap-4">
+                        <h1 className="text-2xl text-gray-300 font-bold italic">{name}</h1>
+                        <h1 className="text-2xl text-gray-500">{year.split("-")[0]}</h1>
+                    </div>
+                    <div className="flex flex-row gap-2 items-center">
+                        <h1 className="text-md text-gray-400 italic">Album by {artist}</h1>
+                        <div className="tooltip" data-tip="Open Spotify">
+                            <a href={spotifyLink} target="_blank">
+                                <img src={SpotifySVG} style={{ width: 15, height: 14 }} className="self-center" />
+                            </a>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
-export default SearchAlbum
+export default SearchAlbum;
--- web/src/pages/SearchResults.jsx
+++ web/src/pages/SearchResults.jsx
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import Titles from "../components/Titles";
 import Loading from "../components/Loading";
-import Album from "../components/Album";
+import SearchAlbum from "../components/SearchAlbum";
 
 function SearchResults() {
     const { state } = useLocation();
@@ -12,11 +12,22 @@ function SearchResults() {
     // const { isLoading: userLoad, data: userData, error: userError } = useQuery([`albums?query=${query}`]);
     
     return(
-        <div className="max-w-6xl mx-auto pt-8">
-            <Titles title={`Search Results for "${query}" in ${type}`} />
+        <div className="max-w-4xl mx-auto pt-8">
+            <Titles title={`${albumData?.data.length} Search Results for "${query}" in ${type}`} />
+            {console.log(albumData?.data)}
             {albumLoad ? <Loading /> :
                 albumData?.data.map(album => {
-                    return <Album album={{img: album.images[0].url, size: "100", name: album.name}}/>
+                    return <>
+                        <SearchAlbum album={{
+                            img: album.images[0].url, 
+                            name: album.name, 
+                            year: album.release_date, 
+                            artist: album.artists[0].name, 
+                            spotifyLink: album.external_urls.spotify
+                        }}/>
+                        
+                        <div className="border-t border-gray-600 max-w-6xl mx-auto" />
+                    </>
                 })
             }
         </div>
