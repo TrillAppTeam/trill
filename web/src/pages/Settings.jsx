dc230829abd8b84c6943fd1cd18acd9ca5768fc3
avwede <avwede@gmail.com>
2023-03-01 16:31:17 -0500

    Remove cover photo in profile settings

--- web/src/pages/Settings.jsx
+++ web/src/pages/Settings.jsx
@@ -30,15 +30,15 @@ function Settings() {
               </p>
             </div>
           </div>
+
           <div className="mt-5 md:col-span-2 md:mt-0">
               <form onSubmit={updateUser}>
                 <div className="shadow sm:overflow-hidden sm:rounded-md">
                   <div className="space-y-6 bg-gray-700 px-4 py-5 sm:p-6">
-                  
                     <div className="grid grid-cols-3 gap-6">
 
                       {/* Name Input */}
-                      <div className="col-span-3 sm:col-span-2">
+                      <div className="col-span-3 sm:col-span-2 pb-3">
                         <label htmlFor="name" className="block text-sm font-bold text-gray-100">
                           Name
                         </label>
@@ -55,7 +55,7 @@ function Settings() {
                       </div>
                     </div>
 
-                    <div>
+                    <div className="pb-3">
                       <label htmlFor="bio" className="block text-sm font-bold text-gray-100">
                         Bio
                       </label>
@@ -71,7 +71,7 @@ function Settings() {
                       </div>
                     </div>
 
-                    <div>
+                    <div className="pb-10">
                       <label className="block text-sm font-bold text-gray-100">Profile Picture</label>
                       <div className="mt-1 flex items-center">
                         <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
@@ -79,55 +79,28 @@ function Settings() {
                             <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                           </svg>
                         </span>
-                        <button
-                          type="button"
-                          className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-bold leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-trillBlue"
-                        >
-                          Change
-                        </button>
-                      </div>
-                    </div>
-
-                    <div>
-                      <label className="block text-sm font-bold text-gray-100">Cover photo</label>
-                      <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
-                        <div className="space-y-1 text-center">
-                          <svg
-                            className="mx-auto h-12 w-12 text-gray-400"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 48 48"
-                            aria-hidden="true"
-                          >
-                            <path
-                              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                              strokeWidth={2}
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>
-                          <div className="flex text-sm text-gray-600">
+                        <div className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-bold leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:ring-2 focus:ring-trillBlue">
                             <label
                               htmlFor="file-upload"
-                              className="relative cursor-pointer rounded-md bg-gray-600 font-bold text-indigo-200 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-200 focus-within:ring-offset-2 hover:text-indigo-300 mx-auto"
+                              className="relative cursor-pointer rounded-md font-bold text-gray-900 focus-within:outline-none  hover:text-blue-500 mx-auto"
                             >
                               <span>Upload a file</span>
                               <input id="file-upload" name="file-upload" type="file" className="sr-only" />
                             </label>
                           </div>
-                          <p className="text-xs text-gray-200">PNG, JPG, GIF up to 10MB</p>
-                        </div>
                       </div>
                     </div>
                   </div>
+
                   <div className="bg-gray-600 px-4 py-3 text-right sm:px-6">
                     <button
                       type="submit"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-trillBlue py-2 px-4 text-sm font-bold text-gray-900 shadow-sm hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-trillBlue py-2 px-4 text-sm font-bold text-gray-700 shadow-sm hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                       Save
                     </button>
                   </div>
+
                 </div>
               </form>
           </div>
