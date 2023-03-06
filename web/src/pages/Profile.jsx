bdd523d19f7b58318c6f8c15c5472eb0bda2926c
DMFlo <dmflorez23@gmail.com>
2023-03-05 20:27:21 -0500

    Followers on other users page update successfully. Key prop fix on profile page

--- web/src/pages/Profile.jsx
+++ web/src/pages/Profile.jsx
@@ -1,8 +1,7 @@
-import React, { useLayoutEffect, useState, useEffect, useMutation } from 'react';
+import React, { useLayoutEffect, useState, useEffect} from 'react';
 import { Link, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import axios from 'axios';
-
  
 // Components
 import Titles from "../components/Titles"
@@ -18,15 +17,51 @@ function Profile() {
         window.scrollTo(0, 0)
     });
 
-    const {user} = useParams();
-    let paramString = '';
-    if (user) {
-        paramString = `?username=${user}`;
+    const { user } = useParams();
+    const paramString = user ? `?username=${user}` : '';
+
+    const [ isFollowing, setIsFollowing ] = useState(false);
+    const { isLoading, data: userData, error: userError } = useQuery([`users${paramString}`]);
+    const { data: following, refetch: refetchFollowing } = useQuery([`follows?type=getFollowing&username=${userData?.data.username}`], {enabled: !!userData});
+    const { data: followers, refetch: refetchFollowers } = useQuery([`follows?type=getFollowers&username=${userData?.data.username}`], {enabled: !!userData});
+    
+    const follow = useMutation(() => { 
+        return axios.post(`https://api.trytrill.com/main/follows?username=${userData?.data.username}`, {}, 
+            { headers: {'Authorization': `Bearer ${localStorage.getItem('access_token')}`}})
+            .then((res) => {
+                return res;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, {onSuccess: () => {refetchFollowing(); refetchFollowers();}});
+
+    const unfollow = useMutation(() => { 
+        return axios.delete(`https://api.trytrill.com/main/follows?username=${userData?.data.username}`, 
+            { headers: {'Authorization': `Bearer ${localStorage.getItem('access_token')}`}})
+            .then((res) => {
+                return res;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }, {onSuccess: () => {refetchFollowing(); refetchFollowers();}});
+
+    useEffect(() => {
+        if (followers?.data.users.includes(localStorage.getItem('username'))) {
+            setIsFollowing(true);
+        } else {
+            setIsFollowing(false);
+        }
+    }, [followers?.data]);
+    
+    const handleFollow = () => {
+        follow.mutate();
     }
 
-    const {isLoading, data: userData, error: userError} = useQuery([`users${paramString}`]);
-    const {data: following, error: followingError} = useQuery([`follows?type=getFollowing&username=${userData?.data.username}`], {enabled: !!userData});
-    const {data: followers, error: followerError} = useQuery([`follows?type=getFollowers&username=${userData?.data.username}`], {enabled: !!userData});
+    const handleUnfollow = () => {
+        unfollow.mutate();
+    }
 
     let albumDummy = { 
         "images": [{"url": "https://i.scdn.co/image/ab67616d0000b2732e8ed79e177ff6011076f5f0"}], 
@@ -51,67 +86,14 @@ function Profile() {
         rating: 5,
     };
 
-    let currentUserDummy = "avwede"
-    const [isFollowing, setIsFollowing] = useState(false);
-
-    // Hook checks if the current user is in the followers object when the component mounts or when the followers.users value changes.
-    // If the current user is found, the initial state of isFollowing is set to true.
-    useEffect(() => {
-        if (following?.data.users.includes(currentUserDummy)) {
-          setIsFollowing(true);
-          console.log("FOLLOWING DATA OBJECT" + following?.data.users);
-        }
-      }, [following?.data.users]);
-
-    const handleFollow = () => {
-        axios({
-            method: 'post',
-            url: `https://api.trytrill.com/main/follows?username=${userData?.data.username}`,
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-            }
-          })
-          .then(res => {
-            console.log(res);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-
-        setIsFollowing(true);
-    }
-
-    const handleUnfollow = () => {
-        // DELETE api request
-        axios({
-            method: 'delete',
-            url: `https://api.trytrill.com/main/follows?username=${userData?.data.username}`,
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${localStorage.getItem('access_token')}`
-            }
-          })
-          .then(res => {
-            console.log(res);
-          })
-          .catch(err => {
-            console.log(err);
-          });
-          
-        setIsFollowing(false);
-    }
-
     return (
         <>
         {isLoading ? <Loading/> : <div className="max-w-5xl mx-auto">
             {/* Profile Section */}
             <div className="flex flex-row flex-wrap py-10 justify-between mx-10">
                 <div className="flex flex-row m-5">
-                    {/* Profile Picture */}
                     <Avatar user={{...userData?.data, size: "24", linkDisabled: true}} />
 
-                    {/* Name and Bio */}
                     <div className="pl-10">
                         <div className="flex gap-2">
                             <h1 className="font-bold text-white text-3xl">{userData?.data.nickname}</h1>
@@ -122,22 +104,22 @@ function Profile() {
                                 </Link> 
                             : 
                                 <>
-                                    {!isFollowing && (
+                                    {isFollowing ? 
                                         <button
-                                        className="btn btn-xs bg-gray-700 hover:bg-trillBlue text-gray-100 hover:text-trillPurple mt-2"
-                                        onClick={handleFollow}
-                                        >
-                                        Follow
-                                        </button>
-                                    )}
-                                    {isFollowing && (
-                                        <button
-                                        className="btn btn-xs bg-trillBlue hover:bg-gray-700 text-trillPurple mt-2 hover:text-gray-100"
-                                        onClick={handleUnfollow}
+                                            className="btn btn-xs bg-trillBlue hover:bg-gray-700 text-trillPurple mt-2 hover:text-gray-100"
+                                            onClick={handleUnfollow}
                                         >
                                         Following
                                         </button>
-                                    )}
+                                        : 
+                                        <button
+                                            className="btn btn-xs bg-gray-700 hover:bg-trillBlue text-gray-100 hover:text-trillPurple mt-2"
+                                            onClick={handleFollow}
+                                        >
+                                        Follow
+                                        </button>
+                                    }
+
                                 </>
                             }
                         </div>
@@ -146,7 +128,6 @@ function Profile() {
                     </div>
                 </div>
               
-                {/* User Statistics */}
                 <div className="pt-5">
                     <UserStats albums={30} followers={followers?.data.users.length} following={following?.data.users.length}/>
                 </div>
@@ -182,7 +163,7 @@ function Profile() {
             <Titles title="Following"/>
             <div className="flex flex-col justify-center items-left max-w-5xl pb-10">
                 <div className="flex gap-2 flex-wrap">
-                    {following?.data.users.map(user => {return <Avatar user={{profilePic: null, username: user, size: '11'}}/>})}
+                    {following?.data.users.map(user => {return <Avatar key={user} user={{profilePic: null, username: user, size: '11'}}/>})}
                 </div>
             </div>
 
@@ -190,7 +171,7 @@ function Profile() {
             <Titles title="Followers"/>
             <div className="flex flex-col justify-center items-left max-w-5xl pb-10">
                 <div className="flex gap-2 flex-wrap">
-                    {followers?.data.users.map(user => {return <Avatar user={{profilePic: null, username: user, size: '11'}}/>})}
+                    {followers?.data.users.map(user => {return <Avatar key={user} user={{profilePic: null, username: user, size: '11'}}/>})}
                 </div>
             </div>
         </div>}
