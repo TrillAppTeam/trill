b3da4730d3aca7e560d92dbc3b1725b727bfe57f
Ashley <avwede@gmail.com>
2023-03-11 12:05:48 -0500

    [BUG FIX] Check if favorite albums is empty before rendering (#88)
    * Add tooltip to user avatars
    
    * Check if favorite albums is empty before mapping

--- web/src/components/Avatar.jsx
+++ web/src/components/Avatar.jsx
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 const AvatarComp = (props) =>{
     const { profilePic, username, size} = props.user;
     return (
-        <div className="avatar placeholder">
-            <div className={`bg-neutral-focus text-white rounded-full ring-2 ring-trillBlue hover:ring-white w-${size} h-${size}`}>
-                { profilePic ? 
-                    <img src={ profilePic } />
-                    :
-                    <span className={size === "24" ? "text-5xl text-white uppercase" : "text-md text-white uppercase"}>
-                        { username ? username[0]: "" }
-                    </span> 
-                }     
+        <div className="tooltip" data-tip={username}>
+            <div className="avatar placeholder">
+                <div className={`bg-neutral-focus text-white rounded-full ring-2 ring-trillBlue hover:ring-white w-${size} h-${size}`}>
+                    { profilePic ? 
+                        <img src={ profilePic } />
+                        :
+                        <span className={size === "24" ? "text-5xl text-white uppercase" : "text-md text-white uppercase"}>
+                            { username ? username[0]: "" }
+                        </span> 
+                    }     
+                </div>
             </div>
         </div>
+       
     );
 }
 
