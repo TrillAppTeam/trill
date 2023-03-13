f5417a39c180398d87daa5b9549aed6088d74d2e
avwede <avwede@gmail.com>
2023-03-13 18:09:27 -0400

    Update new from friends to use new /reviews api

--- web/src/components/NoTextAlbumReview.jsx
+++ web/src/components/NoTextAlbumReview.jsx
@@ -1,33 +1,31 @@
 import { Link } from "react-router-dom"
-import { useQuery } from '@tanstack/react-query';
 import { DateTime } from 'luxon';
 
 // Components
 import Avatar from "./Avatar"
 import Stars from "./Stars"
 import Album from "./Album"
-import trillLogo from "/trillTransparent.png"
 
 function NoTextAlbumReview(props) {
-    const { username, profilePic, created_at, updated_at, review_id, album_id, rating } = props.review;
-    const { isLoading, data: albumData, error: albumError } = useQuery([`albums?albumID=${album_id}`]);
+    const { username, profilePic, created_at, updated_at, rating } = props.review;
+    const { images, name, release_date, artists, external_urls, id } = props.review.album;
+
+    const album = { 
+        images,
+        name,
+        release_date,
+        artists,
+        external_urls,
+        id
+      };
     
     const dateTimeObj = DateTime.fromISO(updated_at ? updated_at : created_at);
-    const formattedDate = dateTimeObj.toFormat('MM/dd')
+    const formattedDate = dateTimeObj.toFormat('MMM dd')
 
     return (
         <div className="relative max-w-[100px]">
             <div className="ring-2 ring-gray-500 inline-block">
-                { !albumData 
-                    ?   <div className="py-[14px]">
-                            <img src={trillLogo} width="150"></img>
-                        </div>
-                    : 
-                    <>
-                        <Album album={{...albumData?.data, size: "150"}}/> 
-                    </>
-                    
-                }
+                <Album album={{...album, size: "150"}}/> 
 
                 <div className="bg-gray-700 px-2 py-2">
                     <div className="text-xs text-left flex flex-row">
@@ -37,8 +35,6 @@ function NoTextAlbumReview(props) {
                                 <p className="text-md text-gray-300 pl-2 font-bold hover:text-trillBlue line-clamp-1 truncate">{username.length > 8 ? username.slice(0, 8) + '...' : username}</p>
                             </Link>
                         </div>
-                        
-
                     </div>
                 </div>
             </div>
