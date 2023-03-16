79623ad32607b566e316f001ca5d38dabacfb50d
DMFlo <70411272+DMFlo@users.noreply.github.com>
2023-03-16 10:47:54 -0400

    User cannot access pages without valid token (#101)

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Loading from "../components/Loading";
@@ -6,7 +6,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from 'axios';
 
 function Home() {
-    const { isLoading, error } = useQuery(['fetchToken'], () => 
+    const navigate = useNavigate();
+    const { isLoading } = useQuery(['fetchToken'], () => 
         axios({
             method: 'post',
             url: 'https://trill.auth.us-east-1.amazoncognito.com/oauth2/token',
@@ -21,8 +22,9 @@ function Home() {
             }}).then((res) => {
                 localStorage.setItem('access_token', res.data.access_token);
                 return res;
-            }),
-        { enabled: !localStorage.getItem('access_token'), refetchOnWindowFocus: false });
+            }).catch((error) => {navigate("/"); return error;}),
+        { enabled: !localStorage.getItem('access_token'), refetchOnWindowFocus: false});
+
     return (
         <div className="bg-trillPurple min-h-screen flex flex-col">
             {isLoading && !localStorage.getItem('access_token') ? <Loading/> : <>
