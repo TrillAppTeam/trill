b731e7388f605574f09ee03c9288c82ea685cb58
CathyChian <chiancathy@gmail.com>
2023-03-20 22:08:36 -0400

    goodbye mobile

--- mobile/android/app/src/main/kotlin/com/example/trill/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.trill
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile/lib/models/user.dart
+++
@@ -1,7 +0,0 @@
-// will need more fields, this is just for testing
-class User {
-  String name;
-  String handle;
-
-  User({required this.name, required this.handle});
-}
