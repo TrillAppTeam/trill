51b1ff0b1e63cd5dc56d5e080c4abfb505493ee6
avwede <avwede@gmail.com>
2023-04-08 17:34:22 -0400

    Change edit/delete review to icons

--- web/src/pages/AlbumDetails.jsx
+++ web/src/pages/AlbumDetails.jsx
@@ -100,8 +100,9 @@ function AlbumDetails() {
 
     const removeReview = () => {
         deleteReview.mutate({rating: rating, review_text: reviewText});
+        setIsEditing(true);
+        setRating(0);
         setReviewText("");
-        setRating(0); 
         setData(null);
     }
 
@@ -214,13 +215,34 @@ function AlbumDetails() {
                             <div className="justify-left">
                                 <AlbumDetailsReview review={myReview} />
                             </div>
-                            
-                            <button 
-                                className="mt-5 btn btn-xs bg-gray-700 hover:bg-trillBlue hover:text-black"
-                                onClick={() => setIsEditing(true)}
-                            >
-                                Edit Review
-                            </button>
+
+                            <div className="flex flex-row gap-5 mt-5">
+                                <div className="tooltip" data-tip="Edit Review">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hover:text-green-500 cursor-pointer" onClick={() => setIsEditing(true)}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                    </svg>
+                                </div>
+                                
+                                <div className="tooltip" data-tip="Delete Review">
+                                    {myReview ? (
+                                        <svg 
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            fill="none" 
+                                            viewBox="0 0 24 24" 
+                                            stroke-width="1.5" 
+                                            stroke="currentColor" 
+                                            className="w-6 h-6 hover:text-red-500 cursor-pointer"
+                                            onClick={() => {
+                                                removeReview();
+                                                setIsEditing(false);
+                                            }}>
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                        </svg>
+                                        
+                                    ) : null}
+                                </div>
+                            </div>
+                           
                         </div>
                         
                     </>
@@ -234,18 +256,21 @@ function AlbumDetails() {
                                     <span className="text-gray-400 font-bold"> You</span>
                                 </p>                                    
                                 
-                                {isLoading ? null : <ReactStars
-                                    key={myReview?.id}
-                                    count={5}
-                                    onChange={ratingChanged}
-                                    size={30}
-                                    isHalf={true}
-                                    emptyIcon={<i className="far fa-star"></i>}
-                                    halfIcon={<i className="fa fa-star-half-alt"></i>}
-                                    fullIcon={<i className="fa fa-star"></i>}
-                                    activeColor="#ffd700"
-                                    value={myReview?.rating / 2 || rating}
-                                />}
+                                {isLoading 
+                                    ? null 
+                                    : <ReactStars
+                                        key={myReview?.id}
+                                        count={5}
+                                        onChange={ratingChanged}
+                                        size={30}
+                                        isHalf={true}
+                                        emptyIcon={<i className="far fa-star"></i>}
+                                        halfIcon={<i className="fa fa-star-half-alt"></i>}
+                                        fullIcon={<i className="fa fa-star"></i>}
+                                        activeColor="#ffd700"
+                                        value={myReview?.rating / 2 || rating}
+                                        />
+                                }
                                 <textarea 
                                     rows="3" 
                                     placeholder="This album is..." 
@@ -265,20 +290,19 @@ function AlbumDetails() {
                                         >
                                         {myReview ? "Save" : "Add Review"}
                                     </button>
-                                    {myReview ? (
-                                        <button 
-                                            type="button" 
-                                            className="py-1 mr-3 w-24 text-sm mt-3 font-bold rounded-md text-gray-900 bg-red-900 hover:text-white"
-                                            onClick={() => {
-                                                removeReview();
-                                                setIsEditing(false);
-                                            }}
+                                   
+                                    {myReview ? <button 
+                                        type="button" 
+                                        className="py-1 mr-3 w-24 text-sm mt-3 font-bold rounded-md text-gray-900 bg-gray-600 hover:text-white"
+                                        onClick={() => {
+                                            setIsEditing(false);
+                                        }}
                                         >
-                                            Delete
+                                            Cancel
                                         </button>
-                                    ) : null}
-
-
+                                        : null
+                                    }
+                                    
                                     <div className="text-gray-600 pt-3">
                                         {addOrUpdateReview.isLoading ? (
                                             "Saving..."
