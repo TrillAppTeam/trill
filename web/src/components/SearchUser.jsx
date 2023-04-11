113643ac62eabbbd69e936710e913788ff418ff9
DMFlo <dmflorez23@gmail.com>
2023-04-11 14:56:16 -0400

    User search follower/following bug

--- web/src/components/SearchUser.jsx
+++ web/src/components/SearchUser.jsx
@@ -5,13 +5,12 @@ import Avatar from "./Avatar"
 
 function SearchUser(props) {
     const { profile_picture, username, size } = props.user;
-    const { data: following} = useQuery([`follows?type=getFollowing&username=${username}`]);
-    const { data: followers} = useQuery([`follows?type=getFollowers&username=${username}`]);
+    const { data: following} = useQuery([`follows?type=getFollowing&username=${username}`],  {retry: 3});
+    const { data: followers} = useQuery([`follows?type=getFollowers&username=${username}`], {retry: 3});
 
     return(
         <div className="flex flex-row flex-wrap py-2 justify-between mx-5">
             <div className="flex flex-row">
-
                 <div className="pt-1">
                     <Avatar user={{profile_picture: profile_picture, username: username, size: size}}/>
                 </div>
