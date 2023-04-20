f9fdd3e82dbd5f395bc0a17c397c31a46c8fe8ba
DMFlo <dmflorez23@gmail.com>
2023-04-20 13:59:31 -0400

    Fixed review still appearing after delete

--- web/src/pages/Settings.jsx
+++ web/src/pages/Settings.jsx
@@ -52,7 +52,7 @@ function Settings() {
           <div className="md:grid md:grid-cols-3 md:gap-6 m-24 max-w-5xl mx-auto">
             <div className="md:col-span-1">
               <div className="px-4 sm:px-0">
-                <h3 className="text-3xl font-bold leading-6 text-white">Trill Profile🎵</h3>
+                <h3 className="text-3xl font-bold leading-6 text-white">Trill Profile 🎵</h3>
                 <p className="mt-5 text-lg text-gray-200 pb-10">
                   Showcase your personality by customizing your profile page.
                 </p>
